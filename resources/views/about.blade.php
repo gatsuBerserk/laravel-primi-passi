c0065acb0ec256b9fd8417df8b49bb8ffe624443
gatsuBerserk <berserkdev@outlook.it>
2022-05-03 15:31:37 +0200

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>About</title>
+</head>
+<body>
+    <pre><a href="{{route('hello')}}">Torna alla Home</a></pre>
+    <div class="info">
+    <h1>{{$lastName}} {{$surname}}</h1> 
+    <h3>{{$student}}</h3>
+    </div>
+    
+</body>
+</html>
